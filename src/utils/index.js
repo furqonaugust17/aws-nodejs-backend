b5384bbd36878e528a11291a294251405e434781
furqonaugust17 <furqonaugustseventeenth@gmail.com>
2025-07-13 19:20:28 +0700

    initial commit

---
+++ src/utils/index.js
@@ -0,0 +1,17 @@
+const mapDBToMode = ({
+    id,
+    title,
+    body,
+    tags,
+    created_at,
+    update_at
+}) => ({
+    id,
+    title,
+    body,
+    tags,
+    createdAt: created_at,
+    updateAt: update_at
+})
+
+module.exports = { mapDBToMode };
